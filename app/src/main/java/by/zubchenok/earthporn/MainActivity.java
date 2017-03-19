03d4a39a1946957630e86deacce7850d428859ff
Anton Zubchenok <1991chester@gmail.com>
2017-03-19 16:47:54 +0300

    Added reading image URLs from response JSON

--- app/src/main/java/by/zubchenok/earthporn/MainActivity.java
+++ app/src/main/java/by/zubchenok/earthporn/MainActivity.java
@@ -1,7 +1,10 @@
 package by.zubchenok.earthporn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImagesAsyncTask task = new ImagesAsyncTask();
+        task.execute("https://www.reddit.com/r/EarthPorn/top/.json?limit=100");
+    }
+
+    private class ImagesAsyncTask extends AsyncTask<String, Void, List<String>> {
+
+        @Override
+        protected List<String> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<String> result = QueryUtils.fetchImageUrls(urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> data) {
+//            mAdapter.clear();
+//            if (data != null && !data.isEmpty()) {
+//                mAdapter.addAll(data);
+//            }
+            getApplicationContext();
+        }
     }
 }
---
+++ app/src/main/java/by/zubchenok/earthporn/QueryUtils.java
@@ -0,0 +1,129 @@
+package by.zubchenok.earthporn;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class QueryUtils {
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    public static List<String> fetchImageUrls(String requestUrl) {
+        URL url = createUrl(requestUrl);
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        List<String> imageUrls = extractFeaturesFromJson(jsonResponse);
+        return imageUrls;
+    }
+
+    private static URL createUrl(String urlString) {
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code " + urlConnection.getResponseCode());
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static final String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private static List<String> extractFeaturesFromJson(String jsonString) {
+
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(jsonString)) {
+            return null;
+        }
+        // Create an empty ArrayList that we can start adding images to
+        ArrayList<String> imageUrls = new ArrayList<>();
+
+        try {
+            JSONObject response = new JSONObject(jsonString);
+            JSONObject data = response.getJSONObject("data");
+            JSONArray children = data.getJSONArray("children");
+            for (int i = 0; i < children.length(); i++) {
+                JSONObject item = children.getJSONObject(i);
+                JSONObject itemData = item.getJSONObject("data");
+                String postHint = itemData.getString("post_hint");
+                if (postHint.equals("image")) {
+                    String url = itemData.getString("url");
+                    imageUrls.add(url);
+                }
+            }
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the JSON results", e);
+        }
+
+        return imageUrls;
+    }
+}
